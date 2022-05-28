f22dcc30604dec054a14dc23898793948770a625
storm-sergey <st.sergey.a@gmail.com>
2022-05-28 16:43:27 +0300

    add script

--- gitfix_all_commit_author.sh
+++ gitfix_all_commit_author.sh
@@ -4,7 +4,7 @@ read -p 'Git author and commiter name: ' name
 read -p 'Git author and commiter email: ' email
 
 git filter-branch -f --env-filter "
-	GIT_AUTHOR_NAME=$name
+  GIT_AUTHOR_NAME=$name
   GIT_AUTHOR_EMAIL=$email
   GIT_COMMITTER_NAME=$name
   GIT_COMMITTER_EMAIL=$email
