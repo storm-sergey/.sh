75809ba7ff6570a0f1b015b59c4527e1ecafc844
storm-sergey <st.sergey.a@gmail.com>
2022-12-20 19:12:55 +0300

    Update send_qemu_vms_json.sh

--- send_qemu_vms_json.sh
+++ send_qemu_vms_json.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 export LC_ALL=C.UTF-8;
 
+DEBUG=false
+HOST="http://10.0.3.214:13579"
+
+# fields list from the command "qm config $vm_id"
 VM_CONFIG_FIELDS=(
   "agent" "boot" "bootdisk" "cipassword" "ciuser"
   "cores" "description" "ide2" "ipconfig0" "memory"
@@ -8,6 +12,7 @@ VM_CONFIG_FIELDS=(
   "scsi0" "scsihw" "smbios1" "sockets" "sshkeys" "vmgenid"
 );
 
+# virtual machine info from the machine agent
 get_vm_host() {
   if [ -z "$(grep "is not running" <<< $1)" ] && [ ! -z "$(grep "out-data" <<< $1)" ] ; then
     host="$(grep "\"out-data\" :" <<< $1 | sed s/"\"out-data\"\ ":// )";
@@ -16,13 +21,20 @@ get_vm_host() {
   fi
 }
 
+# virtual machines info from the node
 vms="";
 vm_ids=$(qm list | awk '{print $1}' | tail +2);
 for vm_id in $vm_ids
 do
   p="{\"vm_id\":\"$vm_id\"}";
-  vm_guest_ip=$(qm guest exec $vm_id -- bash -c "hostname -I")
-  vm_guest_hostname=$(qm guest exec $vm_id -- bash -c "hostname")
+  if [ "$DEBUG" = true ] ; then
+    vm_guest_ip="0.0.0.0"
+    vm_guest_hostname="debug"
+  else
+    # long await operations
+    vm_guest_ip=$(qm guest exec $vm_id -- bash -c "hostname -I")
+    vm_guest_hostname=$(qm guest exec $vm_id -- bash -c "hostname")
+  fi
   p+="$(get_vm_host "$vm_guest_ip" "vm_ip")"
   p+="$(get_vm_host "$vm_guest_hostname" "vm_hostname")"
   
@@ -35,7 +47,33 @@ do
   vms+="$(echo $p | sed -e 's/}{/,/g' - )";
 done
 
-json="$(printf "{\"node_hostname\":\"%s\",\"node_ip\":\"%s\",\"vms_configs\":[%s]}" $(hostname) $(hostname -I) "$(echo $vms | sed -e 's/}{/},{/g' - )")";
+# node hardware info
+hostname="$(hostname)"
+ip="$(hostname -I)"
+cpu_model="$(lscpu | grep -i model\ name: | sed s/Model\ name:// | sed s/\ /_/ | tr -d ' ')"
+cpu_cores="$(lscpu | grep -i ^CPU\(s\): | sed s/CPU\(s\):// | tr -d ' ')"
+ram_gib="$(free -h | awk '{print $2}' | tail +2 | head -1)"
+disks="$(lsblk | grep ^sd | awk '{print $1, $4}' | sed s/\ /_/)"
+raid_controller="$(lspci | grep -i RAID\ bus\ controller: | sed s/........RAID\ bus\ controller:// | tr -d ' ')"
 
-curl -X POST http://127.0.0.1:12345 -H "Content-Type: application/json" --data "$json"
+# empty string checking
+[[ ! "$hostname" ]] && hostname="null"
+[[ ! "$ip" ]] && ip="null"
+[[ ! "$cpu_model" ]] && cpu_model="null"
+[[ ! "$cpu_cores" ]] && cpu_cores="null"
+[[ ! "$ram_gib" ]] && ram_gib="null"
+[[ ! "$disks" ]] && disks="null"
+[[ ! "$raid_controller" ]] && raid_controller="null"
+[[ ! "$vms" ]] && vms="null"
+
+# array distruction
+cpu_model="$(echo -e $cpu_model| sed s/\ /-/)"
+ram_gib="$(echo -e $ram_gib| sed s/\ /-/)"
+disks="$(echo -e $disks| sed s/\ /-/)"
+raid_controller="$(echo -e $raid_controller| sed s/\ /-/)"
+
+# make json
+json="$(printf "{\"node_hostname\":\"%s\",\"node_ip\":\"%s\",\"node_cpu_model\":\"%s\",\"node_cores\":\"%s\",\"node_ram_gib\":\"%s\",\"node_disks\":\"%s\",\"node_raid_controller\":\"%s\",\"vms_configs\":[%s]}" $hostname $ip $cpu_model $cpu_cores $ram_gib $disks $raid_controller "$(echo $vms | sed -e 's/}{/},{/g' - )")";
+
+curl -X POST $HOST -H "Content-Type: application/json" --data "$json"
 #nc -l 12345
