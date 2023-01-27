421016c0435d7bd109cffaf2e19bd69f534f49e7
storm-sergey <st.sergey.a@gmail.com>
2023-01-28 00:36:32 +0300

    Update send_qemu_vms_json.sh

--- send_qemu_vms_json.sh
+++ send_qemu_vms_json.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 export LC_ALL=C.UTF-8;
 
-# random (1 - 30 minutes) sleep for unloading server app
-sleep .$[ (RANDOM % 30) + 1]m
+HOST="http://10.0.19.18:13579"
 
-HOST=""
+
+# random (1 - 20 minutes) sleep for unloading server app
+sleep .$[ (RANDOM % 20) + 1]m
 
 # fields list from the command "qm config $vm_id"
 VM_CONFIG_FIELDS=(
@@ -32,10 +33,11 @@ do
   if [ "$DEBUG" = true ] ; then
     vm_guest_ip="0.0.0.0"
     vm_guest_hostname="debug"
-  else
-    # long await operations
-    vm_guest_ip=$(qm guest exec $vm_id -- bash -c "hostname -I")
-    vm_guest_hostname=$(qm guest exec $vm_id -- bash -c "hostname")
+  # else
+    # all VMs don't have a guest agent
+    # and these next two commands are long await operations:
+    # vm_guest_ip=$(qm guest exec $vm_id -- bash -c "hostname -I")
+    # vm_guest_hostname=$(qm guest exec $vm_id -- bash -c "hostname")
   fi
   p+="$(get_vm_host "$vm_guest_ip" "vm_ip")"
   p+="$(get_vm_host "$vm_guest_hostname" "vm_hostname")"
@@ -43,12 +45,12 @@ do
   vm_conf=$(qm config $vm_id);
   for field in "${VM_CONFIG_FIELDS[@]}"
   do
-    f_fld=$(grep "$field:" <<< $vm_conf | sed s/"$field: "// )
+	  f_fld=$(grep "$field:" <<< $vm_conf | sed s/"$field: "// | tr -d '%' | tr -d '\\' | tr -d '[' | tr -d ']' | tr -d '(' | tr -d ')' | tr -d '/' | tr -d '"')
     [[ ! -z $f_fld ]] && p+="{\"$field\":\"$f_fld\"}"
   done
-  vms+="$(echo $p | sed -e 's/}{/,/g' - )";
+  vms+="$(echo $p | sed -e 's/}{/,/g' )";
 done
-
+# | sed -e 's/\%//g' | sed -e 's/\\//g'
 # node hardware info
 hostname="$(hostname)"
 ip="$(hostname -I)"
@@ -73,9 +75,18 @@ cpu_model="$(echo -e $cpu_model | sed s/\ /-/g)"
 ram_gib="$(echo -e $ram_gib | sed s/\ /-/g)"
 disks="$(echo -e $disks | sed s/\ /-/g)"
 raid_controller="$(echo -e $raid_controller | sed s/\ /-/g)"
+vms="$(echo -e $vms | sed -e 's/}{/},{/g' | sed s/\ /-/g)"
 
 # make json
-json="$(printf "{\"node_hostname\":\"%s\",\"node_ip\":\"%s\",\"node_cpu_model\":\"%s\",\"node_cores\":\"%s\",\"node_ram_gib\":\"%s\",\"node_disks\":\"%s\",\"node_raid_controller\":\"%s\",\"vms_configs\":[%s]}" $hostname $ip $cpu_model $cpu_cores $ram_gib $disks $raid_controller "$(echo $vms | sed -e 's/}{/},{/g' - )")";
+json="$(printf "{\"node_hostname\":\"%s\",\"node_ip\":\"%s\",\"node_cpu_model\":\"%s\",\"node_cores\":\"%s\",\"node_ram_gib\":\"%s\",\"node_disks\":\"%s\",\"node_raid_controller\":\"%s\",\"vms_configs\":[%s]}\n" $hostname $ip $cpu_model $cpu_cores $ram_gib $disks $raid_controller $vms)";
 
-curl -X POST $HOST -H "Content-Type: application/json" --data "$json"
-# echo $json
+#echo $json
+
+response="$(curl -s -X POST $HOST -H "Content-Type: application/json" --data "$json")"
+if [[ $response == '<p>Success</p>' ]] ; then
+  echo 'The script have got the code 200'
+  echo $response
+  exit 0
+fi
+echo "The script have got the resopnse $response"
+exit 204
